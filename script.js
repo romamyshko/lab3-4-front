d22826124694e99b7c39bc4a7442ba45bb97d51f
Roman Myshko <57899283+romamyshko@users.noreply.github.com>
2022-06-13 11:42:23 +0300

    lab3-4

---
+++ script.js
@@ -0,0 +1,10 @@
+const modal23 = document.getElementById("popup-modal");
+const btn1 = document.getElementById("myBtn11");
+const span1 = document.getElementsByClassName("close111")[0];
+
+btn1.onclick = function () {
+  modal23.style.display = "block";
+};
+span1.onclick = function () {
+  modal23.style.display = "none";
+};
